bd4edb877eb4fc45971cd11d3a9efba088b092ee
dhnariyan20-hub <dhnariyan20@gmail.com>
2026-01-31 13:16:29 +0600

    Create utils/logger.js

---
+++ utils/logger.js
@@ -0,0 +1,6 @@
+module.exports = {
+  info: (...msg) => console.log("[ INFO ]", ...msg),
+  warn: (...msg) => console.warn("[ WARN ]", ...msg),
+  error: (...msg) => console.error("[ ERROR ]", ...msg),
+  success: (...msg) => console.log("[ SUCCESS ]", ...msg)
+};
