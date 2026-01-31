82f360b2e8bad07ef6f56a6a0031a6f961638a7e
dhnariyan20-hub <dhnariyan20@gmail.com>
2026-01-31 12:36:19 +0600

    Delete core directory

--- core/permission.js
+++
@@ -1,6 +0,0 @@
-module.exports = (uid, config, level) => {
-  if (level === 0) return true;
-  if (level === 1) return config.ADMINS.includes(uid) || uid === config.OWNER;
-  if (level === 2) return uid === config.OWNER;
-  return false;
-};
