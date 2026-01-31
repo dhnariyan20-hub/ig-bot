1f4098736ecdcf36f1ec29334b93542f0e4e43e8
dhnariyan20-hub <dhnariyan20@gmail.com>
2026-01-31 12:31:23 +0600

    Delete cmds directory

--- cmds/help.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  config: { name: "help", permission: 0 },
-  run: async ({ ig, threadId }) =>
-    ig.entity.directThread(threadId)
-      .broadcastText("Commands:\n!ping\n!uid\n!ai\n!stats")
-};
--- cmds/ping.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  config: { name: "ping", permission: 0 },
-  run: async ({ ig, threadId }) =>
-    ig.entity.directThread(threadId).broadcastText("pong 🐐")
-};
